fa3d385e43350169e275853f27e72753ef77c96f
Yury Loginov <yloginov@swiftcom.uk>
2017-02-20 20:14:05 +0300

    Initial Commit

---
+++ CurrencyRate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyRate
+//
+//  Created by Юрий Логинов on 20.02.17.
+//  Copyright © 2017 Yury Loginov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
